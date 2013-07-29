9c74da4e1d3ede7b62477c837fe8e8eb3644347e
gskielian <gregory.kielian@hackerdojo.com>
2013-07-29 11:13:41 -0700

    Create Heartbeat.ino

---
+++ Heartbeat.ino
@@ -0,0 +1,21 @@
+int brightness;
+void setup()
+{  
+pinMode(9,OUTPUT);    
+}
+
+
+void loop()
+{
+
+  for (brightness=0;brightness<=255; brightness++)
+  {
+    analogWrite(9,brightness);
+    delay(20);
+  }
+  for (brightness=255;brightness>=0; brightness--)  
+  {
+    analogWrite(9,brightness);
+    delay(20);
+  }
+}
